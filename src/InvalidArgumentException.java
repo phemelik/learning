300bf546628e7a852c590bec090abc9e13cfb391
Phillip Hemelik <phillip.hemelik@cofinpro.de>
2020-01-29 13:23:44 +0100

    Bowling-Aufgabe fertig

---
+++ src/InvalidArgumentException.java
@@ -0,0 +1,7 @@
+public class InvalidArgumentException extends Exception {
+
+    public InvalidArgumentException() {
+        super ("Die eingegebene Anzahl der Pins ist nicht valide!");
+    }
+
+}
---
+++ src/Pin.java
@@ -0,0 +1,16 @@
+public class Pin {
+    private static String name = "Pin";
+    private BowlingPin.pinState state = BowlingPin.pinState.STANDING;
+
+    public Pin () {
+
+    }
+
+    public void changeState(BowlingPin.pinState state) {
+        this.state = state;
+    }
+
+    public BowlingPin.pinState getState() {
+        return state;
+    }
+}
